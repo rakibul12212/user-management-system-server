a24d3f87e43313a940cd2ae7e97cde36b482542d
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-05-12 16:42:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+ const express =require('express');
+ const cors= require('cors');
+ const app =express();
+ const port = process.env.PORT || 5000; 
+
+ //middle ware
+ app.use(cors());
+ app.use(express.json());
+
+ const users=[
+    {id:1, name:'rakib', email:'rakib@gmail.com'},
+    {id:2, name:'akib', email:'akib@gmail.com'},
+    {id:3, name:'sakib', email:'sakib@gmail.com'},
+ ]
+
+ app.get('/' ,(req,res)=>{
+    res.send('Users management server is running')
+ })
+
+ app.get('/users', (req,res)=>{
+    res.send(users)
+ })
+
+
+ app.post('/users',(req,res)=>{
+    console.log('post api hitting');
+    console.log(req.body);
+    const newUser =req.body;
+    newUser.id =users.length +1;
+    users.push(newUser);
+    res.send(newUser);
+ })
+
+ app.listen(port,()=>{
+    console.log(`Server is running on PORT :${port}`)
+ })
